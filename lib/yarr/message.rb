5929fa29732a0f2179296b5d08ecd261fe59b862
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-11 16:13:17 +0100

    Factored out Yarr::Message::Command

--- lib/yarr/message.rb
+++ lib/yarr/message.rb
@@ -101,6 +101,96 @@ module Yarr
       end
     end
 
+    class Command
+      include Query
+
+      def handle(ast)
+        ast = stringify_hash_values(ast)
+
+        case ast.keys
+        when [:instance_method]
+          @klass = ast[:instance_method][:class_name]
+          @method = ast[:instance_method][:method_name]
+          handle_instance_method
+
+        when [:class_method] then
+          @klass = ast[:class_method][:class_name]
+          @method = ast[:class_method][:method_name]
+          handle_class_method
+
+        when [:method_name] then
+          @method = ast[:method_name]
+          handle_method_name
+
+        when [:class_name] then
+          @klass = ast[:class_name]
+          handle_class_name
+        end
+      end
+
+      private
+
+      def stringify_hash_values(hash)
+        hash.transform_values do |value|
+          case value
+          when Hash then stringify_hash_values(value)
+          else value.to_s
+          end
+        end
+      end
+
+      %i[handle_instance_method handle_class_method
+         handle_class_name handle_method_name
+      ].each do |sym|
+        define_method(sym) { raise NotImplementedError }
+      end
+    end
+
+    class RiCommand < Command
+      private
+
+      def handle_instance_method
+        result = joined_query(klass: @klass, method: @method, flavour: 'instance')
+
+        if result.count == 1
+          "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
+        else
+          "I found #{result.count} entries matching with class: #{@klass}, instance method: #{@method}"
+        end
+      end
+
+      def handle_class_method
+        result = joined_query(klass: @klass, method: @method, flavour: 'class')
+
+        if result.count == 1
+          "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
+        else
+          "I found #{result.count} entries matching with class: #{@klass}, class method: #{@method}"
+        end
+      end
+
+      def handle_class_name
+        result = klass_query(@klass)
+
+        if result.count == 1
+          "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
+        else
+          "I found #{result.count} entries matching with class: #{@klass}."
+        end
+      end
+
+      def handle_method_name
+        result = method_query(@method)
+
+        case result.count
+        when 0 then "Found no entry that matches method #{@method}"
+        when 1 then "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
+        else
+          "I found #{result.count} entries matching with method: #{@method}. Use &list #{@method} if you would like to see a list"
+        end
+      end
+    end
+
     class Message
       include CommandDispatcher
       include Query
@@ -119,54 +209,7 @@ module Yarr
       private
 
       def ri(ast)
-        case ast
-        when Qo[instance_method: Any]
-          klass = ast[:instance_method][:class_name].to_s
-          method = ast[:instance_method][:method_name].to_s
-
-          result = joined_query(klass: klass, method: method, flavour: 'instance')
-
-          if result.count == 1
-            "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
-          else
-            "I found #{result.count} entries matching with class: #{klass}, instance method: #{method}"
-          end
-
-        when Qo[class_method: Any]
-          klass = ast[:class_method][:class_name].to_s
-          method = ast[:class_method][:method_name].to_s
-
-          result = joined_query(klass: klass, method: method, flavour: 'class')
-
-          if result.count == 1
-            "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
-          else
-            "I found #{result.count} entries matching with class: #{klass}, class method: #{method}"
-          end
-
-        when Qo[class_name: Any]
-          klass = ast[:class_name].to_s
-
-          result = klass_query(klass) # yeah. I know
-
-          if result.count == 1
-            "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
-          else
-            "I found #{result.count} entries matching with class: #{klass}."
-          end
-
-        when Qo[method_name: Any]
-          method = ast[:method_name].to_s
-
-          result = method_query(method)
-
-          case result.count
-          when 0 then "Found no entry that matches method #{method}"
-          when 1 then "https://ruby-doc.org/core-2.5.1/#{result.first[:url]}"
-          else
-            "I found #{result.count} entries matching with method: #{method}. Use &list #{method} if you would like to see a list"
-          end
-        end
+        RiCommand.new.handle(ast)
       end
 
       def what_is(ast)
--- spec/yarr/message_spec.rb
+++ spec/yarr/message_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../spec_helper'
+
 require 'yarr/message'
 require 'yarr/database' # TODO : test double
 
@@ -121,12 +122,107 @@ module Yarr
     RSpec.describe WhatIsCommand do
       let(:ast) { { some_ast: {deep: 1} } }
 
-      describe '.handle' do
+      describe '#handle' do
         subject { WhatIsCommand.new.handle(ast) }
 
         it { is_expected.to start_with('It\'s a(n) ') }
         it { is_expected.to match(/some ast/) }
       end
+    end
+
+    RSpec.describe Command do
+      describe '#stringify_hash_values' do
+        subject do
+          Command.new.send(:stringify_hash_values, { a: 1, b: { c: 2 } })
+        end
+
+        it { is_expected.to eq({ a: '1', b: { c: '2' } }) }
+      end
+
+      describe '#handle' do
+        context 'with instance method' do
+
+          let(:ast) do
+            { instance_method: { class_name: 'Array', method_name: 'size' } }
+          end
+
+          before { allow(subject).to receive(:handle_instance_method) }
+
+          it 'dispatches to #handle_instance_method' do
+            subject.handle(ast)
+            expect(subject).to have_received(:handle_instance_method)
+          end
+
+          it 'sets @klass' do
+            expect { subject.handle(ast) }.to(
+              change { subject.instance_variable_get(:@klass) }.to('Array'))
+          end
+
+          it 'sets @method' do
+            expect { subject.handle(ast) }.to(
+              change { subject.instance_variable_get(:@method) }.to('size'))
+          end
+        end
+      end
+
+      context 'with class method' do
+
+        let(:ast) do
+          { class_method: { class_name: 'File', method_name: 'size' } }
+        end
+
+        before { allow(subject).to receive(:handle_class_method) }
+
+        it 'dispatches to #handle_class_method' do
+          subject.handle(ast)
+          expect(subject).to have_received(:handle_class_method)
+        end
+
+        it 'sets @klass' do
+          expect { subject.handle(ast) }.to(
+            change { subject.instance_variable_get(:@klass) }.to('File'))
+        end
+
+        it 'sets @method' do
+          expect { subject.handle(ast) }.to(
+            change { subject.instance_variable_get(:@method) }.to('size'))
+        end
+      end
+
+      context 'with method name' do
+        let(:ast) { { method_name: 'puts' } }
+
+        before { allow(subject).to receive(:handle_method_name) }
+
+        it 'dispatches to #handle_method_name' do
+          subject.handle(ast)
+          expect(subject).to have_received(:handle_method_name)
+        end
+
+        it 'sets @method' do
+          expect { subject.handle(ast) }.to(
+            change { subject.instance_variable_get(:@method) }.to('puts'))
+        end
+      end
+
+      context 'with class name' do
+        let(:ast) { { class_name: 'IO' } }
+
+        before { allow(subject).to receive(:handle_class_name) }
+
+        it 'dispatches to #handle_class_name' do
+          subject.handle(ast)
+          expect(subject).to have_received(:handle_class_name)
+        end
+
+        it 'sets @klass' do
+          expect { subject.handle(ast) }.to(
+            change { subject.instance_variable_get(:@klass) }.to('IO'))
+        end
+      end
+    end
+
+    RSpec.describe RiCommand do
     end
   end
 end
