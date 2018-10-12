8bc6aafcdb769d82f4fd8f2253ac650fa25a6f22
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-12 11:07:18 +0100

    Truncator specs

---
+++ spec/yarr/message/truncator_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'yarr/message/truncator'
+
+module Yarr
+  module Message
+    RSpec.describe Truncator do
+      let (:short) { Faker::Lorem.paragraph_by_chars(Truncator::MAX_LENGTH - 10) }
+      let (:long) { Faker::Lorem.paragraph_by_chars(Truncator::MAX_LENGTH + 10) }
+
+      describe '.truncate' do
+        context 'with short message' do
+          it 'leaves the message untuched' do
+            expect(subject.truncate(short)).to eq short
+          end
+        end
+
+        context 'with long message' do
+          it 'truncates to length' do
+            expect(subject.truncate(long).length).to be <= Truncator::MAX_LENGTH
+          end
+
+          it 'ends with ...' do
+            expect(subject.truncate(long)).to end_with('...')
+          end
+        end
+      end
+    end
+  end
+end
