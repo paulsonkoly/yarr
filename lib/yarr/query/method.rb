a06a70032ee3cda72116dfaa609d3a7c1a84f0fd
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-13 16:26:23 +0100

    yard docs for query/method

--- lib/yarr/query/method.rb
+++ lib/yarr/query/method.rb
@@ -1,6 +1,17 @@
 module Yarr
   module Query
+    # Methods are stored in the database. Method classes provide an access
+    # interface.
+    #
+    # @example
+    #
+    #    a = Method::Strict.query('size')
+    #    a.count # => 17
+    #    a.first.name # => 'size'
+    #    a.first.flavour # => 'instance'
     module Method
+      # Database object for methods.
+      # For usage examples see {Method}.
       class Base
         def initialize(name, url, flavour, klass)
           @name = name
@@ -8,15 +19,30 @@ module Yarr
           @flavour = flavour
           @klass = klass
         end
+        protected :initialize
 
+        # @!attribute [r] name
+        #   @return [String] the method name
+        # @!attribute [r] url
+        #   @return [String] the method url
+        # @!attribute [r] flavour
+        #   @return [String] either 'instance' or 'class'
+        # @!attribute [r] klass
+        #   @return [Integer] the class_id of the asscociated class
         attr_reader :name, :url, :flavour, :klass
 
+        # @return [String] the name of the method
         def to_s
           @name
         end
       end
 
+      # Queries methods by name using strict lookup rules.
+      # For usage examples see {Method}.
       class Strict < Base
+        # Queries methods by name using strict lookup rules. {Result} is an
+        # Enumerable of {Base} objects.
+        # @param name [String] the name of the method
         def self.query(name:)
           dataset = DB[:methods].where(name: name)
           Result.new(dataset, -> row {
@@ -25,7 +51,12 @@ module Yarr
         end
       end
 
+      # Queries methods using SQL like lookup.
+      # For usage examples see {Method}.
       class Like < Base
+        # Queries methods using SQL like lookup. {Result} is an Enumerable of
+        # {KlassAndMethod} objects.
+        # @param name [String] the name of the method
         def self.query(name:)
           dataset = DB[:classes]
             .join(:methods, class_id: :id)
