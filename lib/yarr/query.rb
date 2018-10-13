c704318179afa5a1837163d78fe97b02b9911f76
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-13 17:39:34 +0100

    more yard docs

--- lib/yarr/query.rb
+++ lib/yarr/query.rb
@@ -3,3 +3,12 @@ require 'yarr/query/result'
 require 'yarr/query/method'
 require 'yarr/query/klass'
 require 'yarr/query/klass_and_method.rb'
+
+module Yarr
+  # OO database interface to our database accesses. Classes provide a class
+  # method .query for instance {Method::Strict.query}. These functions return a
+  # {Result} object which contains instances of the former class,
+  # {Method::Strict} in this case.
+  module Query
+  end
+end
--- lib/yarr/query/result.rb
+++ lib/yarr/query/result.rb
@@ -2,10 +2,14 @@ require 'forwardable'
 
 module Yarr
   module Query
+    # Contains the database objects in an enumerable.
     class Result
       extend Forwardable
       include Enumerable
 
+      # @api private
+      # @param dataset [Sequel::Dataset] query result
+      # @param transformer [Proc] transforms dataset lines into {Query} objects
       def initialize(dataset, transformer)
         @dataset = dataset
         @transformer = transformer
@@ -13,6 +17,8 @@ module Yarr
 
       def_delegator :@dataset, :count
 
+      # Yields database objects from a dataset
+      # @yieldparam object the database object
       def each
         @dataset.each { |data| yield(@transformer.call(data)) }
       end
