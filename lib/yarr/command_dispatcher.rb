50cd8f44efd9324f60d4124309c957cc52f8d357
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-14 10:20:30 +0100

    Reek command_dispatcher

--- lib/yarr/command_dispatcher.rb
+++ lib/yarr/command_dispatcher.rb
@@ -2,6 +2,7 @@ require 'yarr/command'
 require 'qo'
 
 module Yarr
+  # Dispatch logic for commands and command targets
   module CommandDispatcher
     attr_reader :error_message
 
@@ -13,22 +14,22 @@ module Yarr
       when Qo['what_is', Any]
         Yarr::Command::WhatIs.new(ast)
 
-      when Qo['ri', Qo[instance_method: Any]]
+      when Qo['ri', imethod]
         Yarr::Command::RiInstanceMethod.new(ast)
-      when Qo['ri', Qo[class_method: Any]]
+      when Qo['ri', cmethod]
         Yarr::Command::RiClassMethod.new(ast)
-      when Qo['ri', Qo[method_name: Any]]
+      when Qo['ri', method_name]
         Yarr::Command::RiMethodName.new(ast)
-      when Qo['ri', Qo[class_name: Any]]
+      when Qo['ri', class_name]
         Yarr::Command::RiClassName.new(ast)
 
-      when Qo['list', Qo[instance_method: Any]]
+      when Qo['list', imethod]
         Yarr::Command::ListInstanceMethod.new(ast)
-      when Qo['list', Qo[class_method: Any]]
+      when Qo['list', cmethod]
         Yarr::Command::ListClassMethod.new(ast)
-      when Qo['list', Qo[method_name: Any]]
+      when Qo['list', method_name]
         Yarr::Command::ListMethodName.new(ast)
-      when Qo['list', Qo[class_name: Any]]
+      when Qo['list', class_name]
         Yarr::Command::ListClassName.new(ast)
 
       else
@@ -40,6 +41,24 @@ module Yarr
     # true if there was an error with command dispatch
     def error?
       @error
+    end
+
+    private
+
+    def imethod
+      Qo[instance_method: Any]
+    end
+
+    def cmethod
+      Qo[class_method: Any]
+    end
+
+    def method_name
+      Qo[method_name: Any]
+    end
+
+    def class_name
+      Qo[class_name: Any]
     end
   end
 end
