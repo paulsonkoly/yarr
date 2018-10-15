477697f57a4bf947edf32a98e097657b3470732a
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-15 18:02:30 +0100

    fix TEST environment var in environment_spec

--- spec/yarr/environment_spec.rb
+++ spec/yarr/environment_spec.rb
@@ -4,14 +4,14 @@ require 'yarr/environment'
 module Yarr
   RSpec.describe Environment do
     context 'with TEST set to 1' do
-      before { ENV['TEST']='1' }
-
       it { is_expected.to be_test }
       it { is_expected.not_to be_production }
     end
 
     context 'with TEST unset' do
       before { ENV.delete('TEST') }
+      # we need to restore this for other tests
+      after { ENV['TEST'] = '1' }
 
       it { is_expected.not_to be_test }
       it { is_expected.to be_production }
