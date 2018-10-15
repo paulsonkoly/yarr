e1cf999bb734b0bc57347ced9192c7fb94b74b6c
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-16 00:30:42 +0100

    Added log file writing, set the log level to info

--- app/yarr.rb
+++ app/yarr.rb
@@ -8,7 +8,7 @@ bot = Cinch::Bot.new do
   configure do |c|
     c.server = 'irc.freenode.org'
     c.nick = 'rubydoc'
-    c.channels = [ '#ruby' ]
+    c.channels = [ '#rubydoc_test' ]
     c.username = config_file.username
     c.password = config_file.password
   end
@@ -17,5 +17,9 @@ bot = Cinch::Bot.new do
     m.reply yarr.reply_to(match)
   end
 end
+
+bot.loggers.clear
+bot.loggers << Cinch::Logger::FormattedLogger.new(File.open("log/log.txt", "a"))
+bot.loggers.level = :info
 
 bot.start
