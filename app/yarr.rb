96769e0e4d1e598fccd4b66b398e772dddef9698
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-10-25 13:26:43 +0100

    Using ssl & sasl for authentication

--- app/yarr.rb
+++ app/yarr.rb
@@ -9,8 +9,14 @@ bot = Cinch::Bot.new do
     c.server = 'irc.freenode.org'
     c.nick = yconfig.nick
     c.channels = yconfig.channels
+
     c.username = yconfig.username
     c.password = yconfig.password
+    c.port = 6697
+    c.ssl.use = true
+    c.ssl.verify = true
+    c.sasl.username = yconfig.username
+    c.sasl.password = yconfig.password
   end
 
   on :message, /\A& *(.*)\z/ do |m, match|
