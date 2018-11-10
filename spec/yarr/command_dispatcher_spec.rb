560d4db6edf1670597d078404a17806bf410ad1f
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-11-10 09:38:03 +0000

    be_an -> be_a. sounds better in English.

--- spec/yarr/command_dispatcher_spec.rb
+++ spec/yarr/command_dispatcher_spec.rb
@@ -11,32 +11,32 @@ module Yarr
   it 'handles "ast aa"' do
    handler = subject.dispatch({command: 'ast', method_name: 'aa'})
 
-   expect(handler).to be_an Command::AST
+   expect(handler).to be_a Command::AST
   end
 
   context 'with ri command' do
    it 'handles method names' do
     handler = subject.dispatch({command: 'ri', method_name: 'aa'})
 
-    expect(handler).to be_an Command::RiMethodName
+    expect(handler).to be_a Command::RiMethodName
    end
 
    it 'handles class names' do
     handler = subject.dispatch({command: 'ri', class_name: 'Aa'})
 
-    expect(handler).to be_an Command::RiClassName
+    expect(handler).to be_a Command::RiClassName
    end
 
    it 'handles instance methods' do
     handler = subject.dispatch({command: 'ri', instance_method: 'Aa'})
 
-    expect(handler).to be_an Command::RiInstanceMethod
+    expect(handler).to be_a Command::RiInstanceMethod
    end
 
    it 'handles class methods' do
     handler = subject.dispatch({command: 'ri', class_method: 'Aa'})
 
-    expect(handler).to be_an Command::RiClassMethod
+    expect(handler).to be_a Command::RiClassMethod
    end
   end
 
@@ -44,25 +44,25 @@ module Yarr
    it 'handles method names' do
     handler = subject.dispatch({command: 'list', method_name: 'aa'})
 
-    expect(handler).to be_an Command::ListMethodName
+    expect(handler).to be_a Command::ListMethodName
    end
 
    it 'handles class names' do
     handler = subject.dispatch({command: 'list', class_name: 'Aa'})
 
-    expect(handler).to be_an Command::ListClassName
+    expect(handler).to be_a Command::ListClassName
    end
 
    it 'handles instance methods' do
     handler = subject.dispatch({command: 'list', instance_method: 'Aa'})
 
-    expect(handler).to be_an Command::ListInstanceMethod
+    expect(handler).to be_a Command::ListInstanceMethod
    end
 
    it 'handles class methods' do
     handler = subject.dispatch({command: 'list', class_method: 'Aa'})
 
-    expect(handler).to be_an Command::ListClassMethod
+    expect(handler).to be_a Command::ListClassMethod
    end
   end
  end
