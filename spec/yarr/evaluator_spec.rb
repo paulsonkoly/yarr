8ae9e181c103b7d428031ec849fd4c2a176ad0c3
Paul Sonkoly <sonkoly.pal@gmail.com>
2019-01-18 10:08:39 +0000

    Added more coverage  for evaluator

--- spec/yarr/evaluator_spec.rb
+++ spec/yarr/evaluator_spec.rb
@@ -20,39 +20,74 @@ module Yarr
       })
     end
 
-    def configuration_double(output = :truncate, format = "%s")
+    let(:configuration) do
       double 'configuration', {
         evaluator: {
           url: 'http://fake.com',
-          languages: { default: '2.6.0' },
+          languages: {
+            default: '2.6.0',
+            '22' => '2.2.2'
+          },
           modes: {
             default: {
-              format: format,
-              output: output
+              format: "%s",
+              output: :truncate
+            },
+            'ast' => {
+              format: "%s",
+              output: :link,
+              verb: 'cooked'
             }
           }
         }
       }
     end
 
-    def args(code)
+    def args(code, language = '2.6.0')
       {
-        body: %Q({"run_request":{"language":"ruby","version":"2.6.0","code":"#{code}"}}),
+        body: %Q({"run_request":{"language":"ruby","version":"#{language}","code":"#{code}"}}),
         headers: {:"Content-Type"=>"application/json; charset=utf-8"}
       }
     end
 
-    describe '#evaluate normal exppression' do
+    describe '#evaluate normal expression' do
       subject do
-        described_class.new('', web_service, configuration_double)
+        described_class.new('', web_service, configuration)
       end
 
       context 'of 1 + 1' do
         it 'sends the right request to web_service' do
-          expect(web_service).to receive(:post).with('http://fake.com', args('1 + 1'))
+          expect(web_service).to receive(:post)
+            .with('http://fake.com', args('1 + 1'))
           allow(web_service).to receive(:post).and_return(response_double('2'))
+          subject.evaluate('1 + 1')
+        end
 
+        it 'returns the right result' do
+          allow(web_service).to receive(:post).and_return(response_double('2'))
           expect(subject.evaluate('1 + 1')).to eq '# => 2 (http://fake.com/evaluated)'
+        end
+      end
+    end
+
+
+    describe '#evaluate ast' do
+      subject do
+        described_class.new('ast22', web_service, configuration)
+      end
+
+      context 'of 1 + 1' do
+        it 'sends the right request to web_service' do
+          expect(web_service).to receive(:post)
+            .with('http://fake.com', args('1 + 1', '2.2.2'))
+          allow(web_service).to receive(:post).and_return(response_double('2'))
+          subject.evaluate('1 + 1')
+        end
+
+        it 'returns the right result' do
+          allow(web_service).to receive(:post).and_return(response_double('2'))
+          expect(subject.evaluate('1 + 1'))
+            .to eq 'I have cooked your code, the result is at http://fake.com/evaluated'
         end
       end
     end
