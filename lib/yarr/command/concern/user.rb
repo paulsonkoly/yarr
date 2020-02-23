ca7fb5966657fc90dd3ba1c9c94673647b98c648
Paul Sonkoly <sonkoly.pal@gmail.com>
2020-02-23 15:44:36 +0000

    fix, apparently the freenode nick doesn't have a host

--- lib/yarr/command/concern/user.rb
+++ lib/yarr/command/concern/user.rb
@@ -8,7 +8,7 @@ module Yarr
         # Is the user an operator
         # @param user [Cinch::User|NoIRC::User] user
         def op?(user)
-          user.host.match?(/\A#{Yarr.config.ops_host_mask}#{user.nick}\z/)
+          user.host&.match?(/\A#{Yarr.config.ops_host_mask}#{user.nick}\z/)
         end
 
         module_function :op?
